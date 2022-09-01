0bda7c5ff20d34fb56eec926ea4af4d871dc9674
Simon Warchol <simonwarchol@g.harvard.edu>
2022-09-01 15:30:10 -0400

    Adding hw0

---
+++ HW0/README.md
@@ -0,0 +1,22 @@
+# [Tier 1] Programming HW0: Make a To-Do App with just HTML, CSS, and JS
+## Simon Warchol
+
+### How to Run
+Serve the `HW0` directory using any http-server and access `index.html`.
+
+Example:
+```
+npm i -g http-server
+http-server .
+```
+Access assignment at  http://127.0.0.1:8080
+
+### How to Use
+![img.png](img.png)
+Add new items to the list by typing them into the input field and pressing `Add`
+
+Mark items as completed by clicking the checkbox
+
+Edit items by pressing the pencil icon
+
+Delete items by pressing the trash icon
--- HW0/hw0.js
+++ HW0/hw0.js
@@ -1,3 +1,4 @@
+// Base elements created in index.html
 const list = document.getElementById('list');
 const addButton = document.getElementById('add_button');
 const newItem = document.getElementById('new_item');
@@ -5,19 +6,44 @@ const newItem = document.getElementById('new_item');
 addButton.onclick = () => {
     if (newItem?.value !== '') {
         const li = document.createElement('li');
-        li.innerHTML = `<button type="button" class="delete-button">️🗑</button><input type="checkbox" class="checkbox"><span>${newItem.value}</span>`;
-        li.onclick = completeTask;
+        const innerDiv = document.createElement('div');
+        innerDiv.classList.add('task');
+        //Create checkbox, delete button, and label itself and append to li
+        innerDiv.innerHTML = `<div>
+                                  <!--Crosses out item-->
+                                  <input type="checkbox" class="checkbox">
+                                  <!-- The contenteditable property on the span allows it to be edited when toggled  -->
+                                  <span class="task-text" contenteditable="false">${newItem.value}</span>
+                              </div>
+                              <div>
+                                  <!--Toggles contenteditable to change task-->
+                                  <button type="button" class="edit-button" title="Edit Task">️✏️</button>
+                                    <!--Remove task from list-->   
+                                  <button type="button" class="delete-button" title="Delete Task">️🗑</button>
+                              </div>`;
+        // Add listener for click events
+        innerDiv.onclick = clickTask;
+        //Add content to the list item
+        li.appendChild(innerDiv);
+        // Add list item to list
         list.appendChild(li);
+        // BLank out input field
         newItem.value = '';
     }
 }
 
-const completeTask = (e) => {
-    if (e.target.className === 'checkbox') {
+// Clicking on a task either toggles it completed or deletes it
+const clickTask = (e) => {
+    if (e.target.className === 'checkbox') {    // Toggle completed
         // This line was suggested by copilot which toggles this class on and off
         e.target.parentElement.classList.toggle('completed');
-    } else if (e.target.className === 'delete-button') {
-        e.target.parentElement.remove();
+    } else if (e.target.className === 'delete-button') { // Delete task
+        e.target.parentElement.parentElement.remove();
+    } else if (e.target.className === 'edit-button') { // Delete task
+        // Find neighboring parent element
+        const taskText = e.target.parentElement.parentElement.getElementsByClassName('task-text')[0];
+        const contentEditable = taskText.getAttribute('contenteditable');
+        // Toggle between span being editable or not
+        taskText.setAttribute('contenteditable', contentEditable === 'false' ? 'true' : 'false');
     }
-    console.log(e)
 }
