b61095a90bf36720a54e95f2951c01487d0bd521
Simon Warchol <simonwarchol@g.harvard.edu>
2022-10-12 13:44:52 -0400

    Adding firebase

--- Tier2/user-study/src/Context.jsx
+++ Tier2/user-study/src/Context.jsx
@@ -1,4 +1,7 @@
 import React, {useEffect, useState} from "react";
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
 
 export const AppContext = React.createContext(null);
 
@@ -7,9 +10,27 @@ export const AppContext = React.createContext(null);
 
 export const ContextWrapper = (props) => {
 
+
     const [participants, setParticipants] = useState([]);
     const [timeBlocks, setTimeBlocks] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
+    const firebaseConfig = {
+        apiKey: import.meta.env.VITE_API_KEY,
+        authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_APP_ID,
+        measurementId: import.meta.env.VITE_MEASUREMENT_ID
+    };
+    const [firebaseApp, setFirebaseApp] = useState(initializeApp(firebaseConfig));
+    const [db, setDb] = useState(null);
+
+    useEffect(() => {
+        if (!firebaseApp) return
+        setDb(getFirestore(firebaseApp));
+    }, [firebaseApp])
+
 
     return (
         <AppContext.Provider value={
